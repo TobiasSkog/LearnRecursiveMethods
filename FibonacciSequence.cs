c28c4350dd38407181f4378d13df795416cba570
Tobias Skog <t_ott_e@hotmail.com>
2023-10-19 19:00:17 +0200

    Added Fibonacci sequence and a output with the xpected values and the results

--- FibonacciSequence.cs
+++ FibonacciSequence.cs
@@ -2,7 +2,28 @@
 {
     // Fibonacci Sequence (Intermediate):Implement a recursive function to generate
     // the nth number in the Fibonacci sequence. Begin with a small value of n.
-    public class FibonacciSequence
+    public static class FibonacciSequence
     {
+        public static int Fibonacci(int n)
+        {
+            // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+            // F0 = 0, F1 = 1, F2 = 2, F3 = 2, F4 = 3, F5 = 5, F6 = 8, F7 = 13, F8 = 21, F9 = 34
+            // Fn = Fn-1 + Fn-2 for every n > 1
+            // F2 = F1 + F0
+            // F3 = F2 + F1
+            // F4 = F3 + F2
+
+            //  F0 = 0, F1= 1, Fn = Fn-1 + Fn-2, where n > 1.
+
+            // Recursive until we reach 0 and add all values together?
+            if (n > 1)
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+            else
+            {
+                return n;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,50 @@
-﻿namespace LearnRecursiveMethods
+﻿using System.Diagnostics;
+
+namespace LearnRecursiveMethods
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var a = FactorialCalculation.Factorial(7);
-            Console.WriteLine(a);
+            var stopwatch = new Stopwatch();
+            var breaker = "\n-----------------------------------------------\n";
+            var nextCalc = "\n===============================================\n";
+
+
+
+            Console.WriteLine(nextCalc);
+
+            stopwatch.Start();
+            var a = FactorialCalculation.Factorial(4);
+            stopwatch.Stop();
+            Console.WriteLine("\t~~ Factorial Calculation ~~");
+            Console.WriteLine($"\t\t   !4 \n\tExpected result: 21 \n\tResult: {a} \n\tTime spent calculating: {stopwatch.ElapsedMilliseconds}m");
+
+            Console.WriteLine(breaker);
+
+            stopwatch.Restart();
+            a = FactorialCalculation.Factorial(7);
+            stopwatch.Stop();
+            Console.WriteLine($"\t\t   !7 \n\tExpected result: 5040 \n\tResult: {a} \n\tTime spent calculating: {stopwatch.ElapsedMilliseconds}ms");
+
+            Console.WriteLine(nextCalc);
+
+            Console.WriteLine("\t~~ Fibonacci Sequence of nth term ~~");
+            stopwatch.Restart();
+            var b = FibonacciSequence.Fibonacci(6);
+            Console.WriteLine($"\t\t   n = 6 \n\tExpected result: 8 \n\tResult: {b} \n\tTime spent calculating: {stopwatch.ElapsedMilliseconds}ms");
+            stopwatch.Stop();
+
+            Console.WriteLine(breaker);
+
+            stopwatch.Restart();
+            b = FibonacciSequence.Fibonacci(18);
+            Console.WriteLine($"\t\t   n = 18 \n\tExpected result: 2584 \n\tResult: {b} \n\tTime spent calculating: {stopwatch.ElapsedMilliseconds}ms");
+            stopwatch.Stop();
+
+            Console.WriteLine(nextCalc);
+
+
         }
     }
 }
