fea2cfaa6fbdb38d8f3267eef16546a84677effb
Tobias Skog <t_ott_e@hotmail.com>
2023-10-19 18:28:06 +0200

    FactorialCalculation done

--- FactorialCalculation.cs
+++ FactorialCalculation.cs
@@ -3,7 +3,26 @@
     // Factorial Calculation (Easy)
     // Write a recursive function to calculate the factorial of a non-negative
     // integer. Start with small numbers to understand the concept.
-    public class FactorialCalculation
+    public static class FactorialCalculation
     {
+        public static int Factorial(int n)
+        {
+            // n! = n * (n-1) * ... * 1
+            // The factorial function (symbol: !) says to multiply all whole numbers from our chosen number down to 1.
+            // Examples:
+            // 4! = 4 × 3 × 2 × 1 = 24
+            // 7! = 7 × 6 × 5 × 4 × 3 × 2 × 1 = 5040
+            // 1! = 1
+
+            if (n == 1)
+            {
+                return n;
+            }
+
+            n *= Factorial(n - 1);
+
+            return n;
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = FactorialCalculation.Factorial(7);
+            Console.WriteLine(a);
         }
     }
 }
